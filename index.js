4f7136571a5a65188b39b62175f54d78d535c629
glowlog <glowlog@gmail.com>
2023-12-23 14:39:03 +0200

    booster update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ async function fetchPoints() {
   return data;
 }
 
-// Function to update the table with user points
+// Function to update the table with user points and booster status
 async function updateTable() {
   // Get the table element
   const table = document.getElementById("userTable");
@@ -17,21 +17,24 @@ async function updateTable() {
 
   // Create an array of user objects sorted by points in descending order
   const sortedUsers = Object.entries(userPoints)
-    .sort((a, b) => b[1] - a[1])
-    .map(([user, points]) => ({ user, points }));
+    .sort((a, b) => b[1].points - a[1].points)
+    .map(([user, { points, hasBooster }]) => ({ user, points, hasBooster }));
 
   // Loop through the sorted array and dynamically populate the table
-  sortedUsers.forEach(({ user, points }) => {
+  sortedUsers.forEach(({ user, points, hasBooster }) => {
     const row = table.insertRow(-1); // -1 inserts the row at the end of the table
 
-    // Insert user, points, and Twitter profile link (if available) into the row
+    // Insert user, points, hasBooster, and Twitter profile link (if available) into the row
     const cellUser = row.insertCell(0);
     cellUser.textContent = user;
 
     const cellPoints = row.insertCell(1);
     cellPoints.textContent = points;
 
-    const cellLink = row.insertCell(2);
+    const cellHasBooster = row.insertCell(2);
+    cellHasBooster.textContent = hasBooster ? "Yes" : "No";
+
+    const cellLink = row.insertCell(3);
     if (user.startsWith("@")) {
       const twitterLink = `https://twitter.com/${user.slice(1)}`;
       cellLink.innerHTML = `<a href="${twitterLink}">${twitterLink}</a>`;
